186f694811f8eb418f415cf34158a1a236d1ab14
Pachi-Dev <pachi.claros@gmail.com>
2024-01-09 17:20:15 -0600

    Import App component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 import i18n from './i18n'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
